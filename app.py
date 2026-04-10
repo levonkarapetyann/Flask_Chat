166cdcd08cc13196d5c57ceb55af2e60fcc1d25d
Levon <lyov33333@gmail.com>
2026-04-11 00:29:21 +0400

    Add flask-socketio and server

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
+from flask_socketio import SocketIO, join_room
 
 app = Flask(__name__)
+socketio = SocketIO(app, async_mode='threading')
 
 @app.route('/lobby')
 @app.route('/')
@@ -16,5 +18,9 @@ def chat():
     else:
          return render_template('index.html')
 
+@socketio.on('join_room')
+def handle_join_room(data):
+    app.logger.info("{} joined the room: {}".format(data['username'], data['room']))
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
