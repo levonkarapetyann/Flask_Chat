512dd375cbc4592c6655eb5a1b5602b465257b94
Levon <lyov33333@gmail.com>
2026-04-10 22:38:41 +0400

    Add chat.html and redirects to it

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/chat')
+def chat():
+    username = request.args.get('username')
+    room = request.args.get('room')
+    if username and room:
+        return render_template('chat.html', username=username, room=room)
+    else:
+         return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
