ff4e26c66eec7c398daa08e314211035af8dec45
Levon <lyov33333@gmail.com>
2026-04-13 13:43:38 +0400

    Add send message function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, join_room
+from flask_socketio import SocketIO, join_room, send
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='threading')
@@ -18,9 +18,17 @@ def chat():
     else:
          return render_template('index.html')
 
+@socketio.on('send')
+def handle_send(data):
+    app.logger.info("{} sent message to the room {}: {}".format(data['username'], data['room'], data['message']))
+    socketio.emit('give_msg', data)
+
+
 @socketio.on('join_room')
 def handle_join_room(data):
     app.logger.info("{} joined the room: {}".format(data['username'], data['room']))
+    join_room(data['room'])
+    socketio.emit('join_room_notification', data)
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
